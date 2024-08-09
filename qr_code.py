314b4f3c252be18a3f824d6aff985e73952a2051
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-08-09 12:27:49 +0100

    Initial commit

---
+++ qr_code.py
@@ -0,0 +1,5 @@
+# Serve para gerar qrcodes
+import qrcode
+
+image = qrcode.make('https://127.0.0.1:8000')
+image.save('qr.png')
---
+++ restaurant_menu/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# The second part it's going to be display in the front-end and the first part it's going to be used in the back-end
+MEAL_TYPE: tuple = (
+    ('starters', 'Starters'),
+    ('salads', 'Salads'),
+    ('main_dishes', 'Main Dishes'),
+    ('desserts', 'Desserts'),
+)
+
+STATUS: tuple = (
+    (0, 'Unavailable'),
+    (1, 'Available')
+)
+
+
+class Item(models.Model):
+    meal = models.CharField(max_length=500, unique=True)  # Com isto, não vou ter valores duplicados
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(decimal_places=2)
+    # É mais inteligente definir choices, pois vai ser sempre igual, as escolhas são limitadas!!!
+    category = models.CharField(choice=MEAL_TYPE)
+    # This field establishes a many-to-one relationship between the model that contains this field and the `User` model.
+    # This allows you to associate an author with each instance of the model, and retrieve the related `User` instance
+    # when needed.
+    # on_delete permite a possibilidade de remover um User. Isto é, se um certo cozinheiro já não está no restaurante, é
+    # possível retirar todas os seus meals do menu!!
+    # Se tivesse colocado on_delete=models.PROTECT, iria ser impossível apagar the data!!!
+    # on_delete=models.SET_NULL permite colocar Null no author
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUS, default=1)
+
+    def __str__(self):
+        return 'Menu database'
